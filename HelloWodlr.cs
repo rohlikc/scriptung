a7e6fbb33ccb8b5066a4d4aae0f3a1577a9a5e3f
rohlikc <viktorvarmuzka43@gmail.com>
2024-11-04 20:14:23 +0100

    Add files via upload

---
+++ HelloWodlr.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWodlr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Vytiskne text: Hello World!
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
